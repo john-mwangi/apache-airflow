64822a97523703006632e5b50d32cf023e95b13e
John Mwangi <mail.mwangi@gmail.com>
2022-08-23 13:22:16 +0300

    Minor changes.

--- dags/bq_pipeline.py
+++ dags/bq_pipeline.py
@@ -31,16 +31,15 @@ with DAG(
     default_args=default_args,
     catchup=False,
 ) as dag:
-    
+
     # Task 2: print shape of df
-    
+
     print_shape = PythonOperator(
-        task_id="print_shape",
-        python_callable=ap.print_df_shape
+        task_id="print_shape", python_callable=ap.print_df_shape
     )
-    
+
     # Task 3: Import BigQuery table and convert to df
-    
+
     read_bq_table = PythonOperator(
         task_id="read_bq_table",
         python_callable=ap.read_bq_table,
@@ -48,10 +47,11 @@ with DAG(
             "tablename": "transactions_01",
             "datasetid": "fs_test01",
             "projectid": "ph-test-01",
-        })
-    
+        },
+    )
+
     # Task 4: Write to another BigQuery table without transformation
-    
+
     write_bq_table = PythonOperator(
         task_id="write_bq_table",
         python_callable=ap.write_bq_table,
@@ -59,6 +59,7 @@ with DAG(
             "bqprojectid": "ph-test-01",
             "datasetid": "fs_test01",
             "tablename": "transactions_02",
-        })
-    
+        },
+    )
+
     read_bq_table >> print_shape >> write_bq_table
